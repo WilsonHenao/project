587227445c850fd7540279ed175d4afe22dfd2b5
Wilson <wilson.henao@utp.edu.co>
2020-05-04 16:24:24 -0500

    Configuración inicial

---
+++ src/main/java/co/edu/utp/isc/gia/project/data/entity/Users.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package co.edu.utp.isc.gia.project.data.entity;
+
+import java.io.Serializable;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+/**
+ *
+ * @author wilso
+ */
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Users implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    
+    @Column(nullable = false)
+    private String name;
+    
+    @Column(nullable = false)
+    private String username;
+    
+    @Column(nullable = false)
+    private String password;
+    
+    @JoinColumn(nullable = false)
+    @OneToOne
+    private RoleUser roleUser;
+}
